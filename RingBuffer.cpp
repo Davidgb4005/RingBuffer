db785e3d2b374fb449012f03bfa342e593050b87
Dave <davidgb4005@gmail.com>
2025-11-07 13:00:16 -0700

    Initial Commit

---
+++ RingBuffer.cpp
@@ -0,0 +1,187 @@
+#include "RingBuffer.hpp"
+#include <iostream>
+RingBuffer::RingBuffer(int len)
+{
+    buffer = new char[len];
+    start_ptr = buffer;
+    end_ptr = buffer + len;
+    read_ptr = buffer;
+    write_ptr = buffer;
+    buffer_overflow = false;
+    buffer_len = len;
+    data_availible = 0;
+}
+
+RingBuffer::~RingBuffer()
+{
+    delete[] buffer;
+}
+void RingBuffer::ResetBuffer()
+{
+    read_ptr = buffer;
+    write_ptr = buffer;
+    buffer_overflow = false;
+    data_availible = 0;
+    if (debug)
+    {
+        std::cout << "Buffer Reset" << std::endl;
+    }
+}
+int RingBuffer::ReadData(char *c)
+{
+    if (buffer_overflow)
+    {
+        if (debug)
+        {
+            std::cout << "Buffer Overflow" << std::endl;
+        }
+        return BUFFER_OVERFLOW;
+    }
+    if (read_ptr == write_ptr)
+    {
+        if (debug)
+        {
+            std::cout << "No Data" << std::endl;
+        }
+        return NO_DATA;
+    }
+    int msg_len = *read_ptr;
+    int bits_availible;
+    if (msg_len < 1)
+    {
+        if (debug)
+        {
+            std::cout << "Invalid Data" << std::endl;
+        }
+        return INVALID_DATA;
+    }
+
+    if (data_availible < msg_len)
+    {
+        if (debug)
+        {
+            std::cout << "Incomplete Data" << std::endl;
+        }
+        return INCOMPLETE_DATA;
+    }
+    else
+    {
+        read_ptr++;
+        data_availible--;
+        if (read_ptr == end_ptr)
+            read_ptr = start_ptr;
+        int i;
+        for (i = 0; i < msg_len; i++)
+        {
+            *c = *(read_ptr);
+            c++;
+            read_ptr++;
+            data_availible--;
+            if (read_ptr == end_ptr)
+                read_ptr = start_ptr;
+            if (data_availible < 0)
+            {
+                if (debug)
+                {
+                    std::cout << "Buffer Overread Data" << std::endl;
+                }
+                return BUFFER_OVERREAD;
+            }
+        }
+        return i;
+    }
+    if (debug)
+    {
+        std::cout << "Unexpected Error" << std::endl;
+    }
+    return UNEXPECTED_ERROR;
+}
+
+int RingBuffer::WriteData(char *c, int len)
+{
+    if (buffer_overflow)
+    {
+        return BUFFER_OVERFLOW;
+    }
+    if (buffer_len < data_availible + len)
+    {
+        buffer_overflow = true;
+        if (debug)
+        {
+            std::cout << "Buffer Overflow" << std::endl;
+        }
+        return BUFFER_OVERFLOW;
+    }
+    else
+    {
+        int i;
+        for (i = 0; i < len; i++)
+        {
+            *write_ptr = *c;
+            c++;
+            write_ptr++;
+            data_availible++;
+            if (write_ptr == end_ptr)
+            {
+                write_ptr = start_ptr;
+            }
+        }
+        return i;
+    }
+    if (debug)
+    {
+        std::cout << "Unexpected Error" << std::endl;
+    }
+    return UNEXPECTED_ERROR;
+}
+
+void RingBuffer::PrintData()
+{
+    int len = 0;
+    char *temp_read_ptr = read_ptr;
+    int temp_data_available = data_availible;
+    if (temp_data_available < 1)
+    {
+        std::cout << "No Data" << std::endl;
+        return;
+    }
+    char c;
+    while (temp_data_available > 0)
+    {
+        len = *temp_read_ptr;
+        temp_read_ptr++; // skip length prefix
+        temp_data_available--;
+        if (len > temp_data_available)
+        {
+            std::cout << "Data Fragment(" << (temp_data_available) << "/" << static_cast<int>(len) << ") : ";
+        }
+        else
+        {
+            std::cout << "Data Length(" << static_cast<int>(len) << ") : ";
+        }
+        for (int k = 0; k < len; ++k)
+        {
+            c = *temp_read_ptr;
+            std::cout << c;
+            temp_read_ptr++;
+            temp_data_available--;
+            if (temp_read_ptr > end_ptr)
+            {
+                temp_read_ptr = start_ptr;
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+void RingBuffer::PrintMsg(char *c, int len)
+{
+    for (int k = 0; k < len; k++)
+    {
+        std::cout << c[k];
+    }
+    std::cout << std::endl;
+}
+void RingBuffer::Debug(bool en)
+{
+    debug = en;
+}
---
+++ RingBuffer.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+class RingBuffer
+{
+public:
+    // Constructors
+    RingBuffer(int len);
+    ~RingBuffer();
+    // Resets
+    void ResetBuffer();// Resets All Errors And Resets (read_ptr) And (write_ptr) To (start_ptr)
+
+    // Memeber Functions
+    int ReadData(char *c);// Read 1 Complete Message From (This->buffer) and copy it into (c)
+    int WriteData(char *c, int len);// Write (len) Bytes From (c) Into (This->buffer)
+
+    // Debugging Static Functions
+    static void PrintMsg(char *c, int len); // Prints (len) Bytes From Pointer (c)
+
+    // Debugging Memeber Functions
+    void Debug(bool en);
+    void PrintData(); // Prints All Bytes In Buffer DOES NOT CONSUME!
+
+    // Variables
+    int data_availible; // Amount Of Valid Bytes In Buffer
+    bool debug;         // Debug enabled Flag
+    char *buffer;       // Ring Buffer
+    // Enums
+    enum Error
+    {
+        NO_DATA = 0,
+        INCOMPLETE_DATA = -1,
+        BUFFER_OVERFLOW = -2,
+        INVALID_DATA = -3,
+        BUFFER_OVERREAD = -4,
+        UNEXPECTED_ERROR = -99
+    };
+
+private:
+    char *read_ptr;
+    char *write_ptr;
+    char *end_ptr;
+    char *start_ptr;
+    int buffer_len;
+    bool buffer_overflow;
+};
